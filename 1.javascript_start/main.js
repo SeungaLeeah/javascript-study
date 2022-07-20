5ff1c1ae851dd20378c33257068f10eacc5c6568
SeungaLeeah <92766716+SeungaLeeah@users.noreply.github.com>
2022-07-20 16:44:38 +0900

    js-study

---
+++ 1.javascript_start/main.js
@@ -0,0 +1,8 @@
+//javascript 사용시, 실수를 체크해줌
+// 순수 자바스크립트를 사용시 use strict를 사용해야함
+'use strict';
+
+console.log('HELLO WORLD');
+let a;
+a = 6;
+console.log(a);
---
+++ 1.javascript_start/tempCodeRunnerFile.js
@@ -0,0 +1,3 @@
+console.log('HELLO WORLD');
+let a;
+a = 6;
